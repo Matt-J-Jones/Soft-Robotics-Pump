e33a9b621ac4eb0e49d60b34f50444f890c828b0
Matt-J-Jones <aratinlondon@gmail.com>
2021-06-22 20:03:39 +0100

    Update Motor.ino
    Updated Code, Version 2

--- Motor.ino
+++ Motor.ino
@@ -1,24 +1,27 @@
+
+const int BUTTON_PIN = 7; // Arduino pin connected to button's pin
+
+int lastButtonState;    // the previous state of button
+int currentButtonState; // the current state of button
 int motorPin = 3;
- 
-void setup() 
-{ 
-  pinMode(motorPin, OUTPUT);
-  Serial.begin(9600);
-  while (! Serial);
-  Serial.println("Speed 0 to 255");
-} 
- 
- 
+
+void setup() {
+  Serial.begin(9600);                // initialize serial
+  pinMode(BUTTON_PIN, INPUT_PULLUP); // set arduino pin to input pull-up mode
+  currentButtonState = digitalRead(BUTTON_PIN);
+}
+
 void loop() 
-{ 
-  if (Serial.available())
+{
+  lastButtonState    = currentButtonState;      // save the last state
+  currentButtonState = digitalRead(BUTTON_PIN); // read new state
+  if(lastButtonState == HIGH && currentButtonState == LOW) 
   {
-    //int speed = Serial.parseInt();
-    int speed = 1;
-    if (speed >= 0 && speed <= 255)
-    {
-      analogWrite(motorPin, speed);
-      
-    }
+    analogWrite(motorPin, 255);
   }
-} 
+  else if(lastButtonState == LOW && currentButtonState == HIGH) 
+  {
+    analogWrite(motorPin, 0);
+  }
+
+}
